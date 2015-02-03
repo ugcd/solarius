169e399cefe43d5de36cf0960f3e792efcc2c7b2
variani <you@example.com>
2015-02-03 11:20:31 -0500

    gait1 changes

--- projects/01-gait1/R/01-assoc-bmi.R
+++ projects/01-gait1/R/01-assoc-bmi.R
@@ -5,16 +5,13 @@ load_all("~/git/ugcd/solarius")
 library(gait1)
 
 ### par
-cores <- 2
+cores <- 64
 
-#pdir <- "/home/datasets/GAIT1/GWAS/SFBR"
-pdir <- "/home/andrey/Datasets/GAIT1/SFBR/subset"
+pdir <- "/home/datasets/GAIT1/GWAS/SFBR"
+#pdir <- "/home/andrey/Datasets/GAIT1/SFBR/subset"
 
 phefile <- file.path(pdir, "gait1.phe")
 pedfile <- file.path(pdir, "gait1.ped")
-
-gdir1 <- file.path(pdir, "Impute/c1.1.500/")
-gdir2 <- file.path(pdir, "Impute/c1.501.1000/")
 
 ### var
 #gait1.snpfiles <- gait1.snpfiles(chr = 1, num.snpdirs = 2)
@@ -24,20 +21,25 @@ gait1.snpfiles <- gait1.snpfiles()
 #gait1.snpfiles <- gait1.snpfiles(chr = 21:22)
 
 ### read phen. data
-pdat <- readPhen(phen.file, sep.phen = "\t", ped.file = ped.file)
+pdat <- readPhen(phefile, sep.phen = "\t", ped.file = pedfile)
 
+pdat <- mutate(pdat,
+  ln_bmi = log(bmi),
+  tr_bmi = 6.1 * ln_bmi)
+  
 ### polygenic model
-M <- solarPolygenic(bmi ~ 1, pdat)
+#M <- solarPolygenic(bmi ~ 1, pdat)
+M <- solarPolygenic(tr_bmi ~ AGE, pdat, covtest = TRUE)
+
 
 ## assoc. model
-genocov.files <- c(file.path(gdir1, "snp.genocov"), file.path(gdir2, "snp.genocov"))
-snplists.files <- c(file.path(gdir1, "snp.geno-list"), file.path(gdir2, "snp.geno-list"))
-snpmap.files <- c(file.path(gdir1, "map.c1.1.500"), file.path(gdir2, "map.c1.501.1000"))
-
 #A <- solarAssoc(bmi ~ 1, pdat, genocov.files = genocov.files[1], snplists.files = snplists.files[1], snpind = 1:4)
 #A <- solarAssoc(bmi ~ 1, pdat, genocov.files = genocov.files[1], snplists.files = snplists.files[1], cores = 2)
 #A <- solarAssoc(bmi ~ 1, pdat, genocov.files = genocov.files, snplists.files = snplists.files)
 #A <- solarAssoc(bmi ~ 1, pdat, genocov.files = genocov.files, snplists.files = snplists.files, snpmap.files = snpmap.files, cores = 2)
 
-A <- solarAssoc(bmi ~ 1, pdat, genocov.files = gait1.snpfiles$genocov.files, snplists.files = gait1.snpfiles$snplists.files, snpmap.files = gait1.snpfiles$snpmap.files, cores = cores)
+A1 <- solarAssoc(tr_bmi ~ 1, pdat, genocov.files = gait1.snpfiles$genocov.files, snplists.files = gait1.snpfiles$snplists.files, snpmap.files = gait1.snpfiles$snpmap.files, cores = cores)
 
+A2 <- solarAssoc(tr_bmi ~ AGE, pdat, genocov.files = gait1.snpfiles$genocov.files, snplists.files = gait1.snpfiles$snplists.files, snpmap.files = gait1.snpfiles$snpmap.files, cores = cores)
+
+save(A1, A2, file = "gait1.bmi.A.RData")
