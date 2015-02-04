5e250108bbc793ed4d2ee8bf8558bd0172ae1763
variani <machovariani@gmail.com>
2015-02-04 12:14:32 +0100

    added loadExamplesPhen function

--- R/data.lib.R
+++ R/data.lib.R
@@ -2,19 +2,58 @@
 # Data loaders
 #----------------------------------
 
-#' Function loadMulticData.
+package.file <- function(...)
+{
+  path <- system.file(...)
+  if(!file.exists(path)) {
+    path <- system.file("inst", ...)
+  }
+  stopifnot(file.exists(path))
+  
+  return(path)
+}
+
+#' Function loadMulticPhen.
 #'
 #' @export
-loadMulticData <- function()
+loadMulticPhen <- function()
 {
-  dat.dir <- system.file("inst", "extdata", "solarOutput", package = "solarius")
-  if(!file.exists(dat.dir)) {
-    dat.dir <- system.file("extdata", "solarOutput", package = "solarius")
-  }
-  stopifnot(file.exists(dat.dir))
+  dat.dir <- package.file("extdata", "solarOutput", package = "solarius")
   
   dat <- readPhen(phen.file = file.path(dat.dir, "simulated.phen"), sep.phen = ",", 
     ped.file = file.path(dat.dir, "simulated.ped"), sep.ped = ",")
+  
+  return(dat)
+}
+
+
+#' Function loadExamplesPhen.
+#'
+#' @export
+loadExamplesPhen <- function(dat.dir)
+{ 
+  ### dir
+  is.tmpdir <- missing(dat.dir)
+  
+  if(is.tmpdir) {
+    dir <- tempfile(pattern = "loadExamplesPhen-")
+    stopifnot(dir.create(dir))
+  }
+  
+  ### run solar
+  if(is.tmpdir) {
+    ret <- solar("example", dir)
+    dat.dir <- dir
+  }
+  
+  ### read files
+  dat <- readPhen(phen.file = file.path(dat.dir, "gaw10.phen"), sep.phen = ",", 
+    ped.file = file.path(dat.dir, "gaw10.ped"), sep.ped = ",")
+  
+  ### clean
+  if(is.tmpdir) {
+    unlink(dir, recursive = TRUE)
+  }
   
   return(dat)
 }
