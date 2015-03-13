740efc79310bd63c57e55d51aacab35d9d801391
variani <machovariani@gmail.com>
2015-03-13 18:03:09 +0100

    documenting solarPolygenic

--- R/solarPolygenic.R
+++ R/solarPolygenic.R
@@ -1,14 +1,46 @@
 #' Function solarPolygenic.
 #'
 #' @param formula
+#'  an object of class \code{formula} or one that can be coerced to that class.
+#'  It is a symbolic description of fixed effects (covariates) to be fitted. 
+#'  If the model does not have any covariates, then the formula looks like 
+#'  \code{trait ~ 1}, where \code{1} means the trait mean parameter.
 #' @param data
+#'    A data frame containing the variables in the model, 
+#'    including ID fields needed to construct random effects: genetic and house-hold (both optional).
+#'    Other classes such as list, environment or object coercible by \code{as.data.frame} to a data frame
+#'    are not supported.
 #' @param dir
+#'    an optional character string, the name of directory,
+#'    where SOLAR performs the analysis.
+#'    In this case, the analysis within related input/output files is 
+#'    conducted in the given folder instead of a temporary one
+#'    (the default work flow).
 #' @param kinship
+#'    A matrix of the kinship coefficient (custom kinship matrix). 
+#'    The IDs are required to be in row and column names.
+#'    Currently, it does not work for unrelated individuals (SOLAR issue).
 #' @param traits
-#' @param covlist The default value is \code{"1"}.
-#' @param covtest The default value is \code{FALSE}.
-#' @param screen The default value is \code{FALSE}.
+#'    a vector of characters to specify trait(s) in the model. It is alternative to the formula interface.
+#' @param covlist 
+#'    a vector of characters to specify fixed effects (covariates) in the model.
+#'    It is alternative to the formula interface.
+#'    It could be convenient to indicate covariates in the SOLAR format,
+#'    for example, \code{"age^1,2,3#sex"} that means \code{sex age agesex age^2 age^2sex age^3 age^3*sex}.
+#'    The default value is \code{"1"}.
+#' @param covtest 
+#'    a logical value, indicating where to test the significance of the fixed effects (covariates).
+#'    Likelihood ratio test (LRT) is used by SOLAR.
+#'    \code{polygenic} SOLAR command is called with a combination of \code{-screen -all} options.
+#'    As a result, \code{cf} slot will have p-values in \code{pval} column.
+#'    The default value is \code{FALSE}.
+#' @param screen 
+#'    a logical value, indicating where to screen the fixed effects (covariates).
+#'    \code{polygenic} SOLAR command is called with \code{-screen} option.
+#'    As a result, only significant covariates will be maintained in the model.
+#'    The default value is \code{FALSE}.
 #' @param household The default value is \code{as.logical(NA)}.
+#'    
 #' @param transforms The default value is \code{character(0)}.
 #' @param alpha The default value is \code{0.05}.
 #' @param polygenic.settings The default value is \code{""}.
