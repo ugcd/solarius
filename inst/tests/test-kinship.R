56d348338234f6976ae30a0d5d813f40d386366c
variani <machovariani@gmail.com>
2015-05-15 15:15:43 +0200

    up vigs

--- inst/tests/test-kinship.R
+++ inst/tests/test-kinship.R
@@ -1,26 +1,6 @@
 
 context("kinship matrix")
 
-test_that("solarKinship2", {
-  data(dat30)
-  
-  kin2 <- solarKinship2(dat30)
-  kin <- kin2 / 2
-
-  dat30.subset <- subset(dat30, select = c("id", "trait1", "sex"))
-  dat30.subset <- mutate(dat30.subset,
-    famid = 0, fa = 0, mo = 0)
-  #dat30.subset <- dat30
-    
-  mod1 <- solarPolygenic(trait1 ~ 1, dat30)
-  mod2 <- solarPolygenic(trait1 ~ 1, dat30.subset, kinship = kin)
-  
-  h2r1 <- with(mod1$vcf, Var[varcomp == "h2r"])
-  h2r2 <- with(mod2$vcf, Var[varcomp == "h2r"])
-
-  expect_equal(h2r1, h2r2)
-})
-
 test_that("two custom kinship matrices", {
   data(dat30)
   
@@ -35,3 +15,4 @@ test_that("two custom kinship matrices", {
 
   expect_more_than(h2r1, h2r2)
 })
+
--- inst/tests/test-solarAssoc.R
+++ inst/tests/test-solarAssoc.R
@@ -11,14 +11,3 @@ test_that("input data parameters `genocovdata` and `genodata`", {
   
   expect_equal(mod1$snpf$pSNP, mod2$snpf$pSNP)
 })
-
-test_that("different results for different kinship matrices", {
-  data(dat50)
-  snps <- 1:2
-  
-  mod1 <- solarAssoc(trait ~ 1, phenodata, snpdata = genodata[, snps], kinship = kin)
-  mod2 <- solarAssoc(trait ~ 1, phenodata, snpcovdata = genocovdata[, snps], kinship = 2*kin)
-  
-  expect_true(all((mod1$snpf$pSNP != mod2$snpf$pSNP)))
-})
-
