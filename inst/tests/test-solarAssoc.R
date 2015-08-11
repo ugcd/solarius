8c20f7480f64f5f28620961186dca3f7c686f016
variani <machovariani@gmail.com>
2015-08-11 11:50:55 +0200

    up tests

--- inst/tests/test-solarAssoc.R
+++ inst/tests/test-solarAssoc.R
@@ -11,3 +11,14 @@ test_that("input data parameters `genocovdata` and `genodata`", {
   
   expect_equal(mod1$snpf$pSNP, mod2$snpf$pSNP)
 })
+
+test_that("input data parameters 2 `genocov.files` & 2 `snplists.files`", {
+  data(dat50)
+  
+  dir <- system.file("inst/extdata/solarAssoc/", package = "solarius")
+  genocov.files <- file.path(dir, c("snp.genocov2", "snp.genocov3"))
+  snplists.files <- file.path(dir, c("snp.geno-list2", "snp.geno-list3"))
+
+  mod <- solarAssoc(trait ~ 1, phenodata, genocov.files = genocov.files, snplists.files = snplists.files)
+  expect_true("solarAssoc" %in% class(mod))
+})
