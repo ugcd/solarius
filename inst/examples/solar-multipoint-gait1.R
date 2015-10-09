331d5cbc56bfa291ea85368155f296498e65ab09
variani <machovariani@gmail.com>
2015-10-09 15:06:07 +0200

    up

--- inst/examples/solar-multipoint-gait1.R
+++ inst/examples/solar-multipoint-gait1.R
@@ -1,6 +1,8 @@
 #library(gait)
 load_all("~/git/ugcd/gait")
 
+library(plyr)
+
 dir <- tempfile(pattern = "solar-multipoint")
 dir <- "solar"
 
@@ -18,20 +20,23 @@ dat <- mutate(dat,
 mibddir <- gait1.mibddir()
 
 ### expclude FAMIDs
-fams <- c("02", "17", "18")
+#fams <- c("02", "17", "18")
 #dat <- subset(dat, !(FAMID %in% fams))
 
 ### export data
 df2solar(dat, dir) 
 
+#f <- list.files("/home/andrey/Datasets/GAIT1/mibdPedigree/", full.names = TRUE)
+#ret <- file.copy(f, dir)
+
 ### run solar 1
 cmd <- c(
   "pedigree load dat.ped", 
   "phen load dat.phe",
-  "model new", "trait tr1_FVII", "covariate MIBD()", "polygenic")
+  "model new", "trait tr1_FVII", 
+  #"covariate MIBD()", 
+  "polygenic")
 ret <- solar(cmd, dir)
-
-stop()
 
 ### run solar 2
 cmd <- c("load model tr1_FVII/null0.mod",
@@ -43,3 +48,35 @@ ret <- solar(cmd, dir)
 
 ### clean
 #unlink(dir, recursive = TRUE)
+
+ibdids <- c("27", "28", "30", "31", "42", "43", "364", "365", "393")  
+pf <- read_pedindex("solar/pedindex.out")
+ids <- with(pf, ID[IBDID %in% ibdids])
+stopifnot(length(ids) == length(ibdids))
+
+mf.extra <- data.frame(ID1 = ids, ID2 = ids, matrix1 = 1, matrix2 = 1)
+
+files <- head(list.files(mibddir, full.names = TRUE))
+for(f in files) {
+  mf <- read_mibd_csv_gz(f)
+  mf <- rbind(mf, mf.extra)
+  
+  of <- file.path(dir, basename(f))
+  
+  z <- gzfile(of, "w")  # compressed file
+  #cat("TITLE extra line", "2 3 5 7", "", "11 13 17", file = zz, sep = "\n")
+  write.table(mf, z, sep = ",", quote = FALSE,
+    row.names = FALSE, col.names = TRUE)
+  close(z)
+}
+
+### run solar 3
+cmd <- c("load model tr1_FVII/null0.mod",
+  paste("mibddir", "."), 
+  "chromosome 10", "interval 1", 
+  "multipoint -overwrite")
+
+ret <- solar(cmd, dir)
+
+
+
